398359c7eaa0bb0dac53d6188f41036415f4f789
piotrekpasciak <piotrek.pasciak@gmail.com>
2021-08-05 02:34:40 +0200

    Rename badges hash field to chainpoint_hash to resolve conflict with ActiveRecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_220005) do
+ActiveRecord::Schema.define(version: 2021_08_05_003152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2021_08_02_220005) do
     t.date "issue_date", null: false
     t.uuid "uuid", null: false
     t.uuid "proof_id", null: false
-    t.string "hash", null: false
+    t.string "chainpoint_hash", null: false
     t.jsonb "metadata", default: "{}", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["hash"], name: "index_badges_on_hash", unique: true
+    t.index ["chainpoint_hash"], name: "index_badges_on_chainpoint_hash", unique: true
     t.index ["proof_id"], name: "index_badges_on_proof_id", unique: true
     t.index ["uuid"], name: "index_badges_on_uuid", unique: true
   end
--- spec/factories/badge_factory.rb
+++ spec/factories/badge_factory.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :badge do
-    recipient_name { 'Jhon' }
-    issue_date    { 1.day.ago.to_date }
-    uuid          { SecureRandom.uuid }
-    proof_id      { SecureRandom.uuid }
-    hash          { SecureRandom.hex(32) }
-    metadata      { { hash_received: 1.day.ago } }
+    recipient_name  { 'Jhon' }
+    issue_date      { 1.day.ago.to_date }
+    uuid            { SecureRandom.uuid }
+    proof_id        { SecureRandom.uuid }
+    chainpoint_hash { SecureRandom.hex(32) }
+    metadata        { { hash_received: 1.day.ago } }
   end
 end
